fe2fa69aae911524e71267b37fd354a877043966
Tim Hildering <hilderingt@posteo.net>
2019-05-14 02:31:12 +0200

    geändert:       lockbdev.sh

--- lockbdev.sh
+++ lockbdev.sh
@@ -1,12 +1,94 @@
 #!/bin/sh
 
-tag="lockbdev"
 conf_file="${rootmnt}/etc/lockbdev.conf"
+log_tag="lockbdev"
+udev_dir="/dev"
 list_ignore=""
 list_lock=""
-udev_dir="/dev"
+bdev_disk=""
 optval=""
+blkdev=""
 bdev=""
+opt=""
+
+parse_bdev()
+{
+	local dev_dir="" bdev_disk="" blkid_opt=""
+
+	bdev=${1}
+
+	case "${bdev}" in
+		disk=*)
+			bdev=${bdev#disk=}
+			bdev_disk="y"
+			;;
+	esac
+
+	case "${bdev}" in
+		UUID=*)
+			bdev=${bdev#UUID=}
+			blkid_opt="-U" 
+			;;
+		LABEL=*)
+			bdev=${bdev#LABEL=}
+			blkid_opt="-L" 
+			;;
+		/*)
+			dev_dir="${bdev%/*}"
+			bdev="${bdev##*/}"
+			;;
+		*)
+			dev_dir="${udev_dir}" ;;
+	esac
+
+	case "${blkid_opt}" in
+		-L | -U)
+			bdev=$(blkid ${blkid_opt} ${bdev})
+
+			if [ ${?} -ne 0 ]
+			then
+				log_warning_msg "${log_tag}: failed to get block device for '${bdev}'" 1>&2
+			fi
+			;;
+	esac
+
+	if ! [ "x${bdev}" = "x" ]
+	then
+		if [ "x${bdev_disk}" = "xy" ]
+		then
+			for blkdev in $(cd /sys/block; echo *)
+			do
+				case "${bdev}" in
+					${blkdev}*)
+						if [ -d "/sys/block/${blkdev}/${bdev}" ]
+						then 
+							bdev_disk="${dev_dir}/${blkdev}"
+						else
+							continue
+						fi
+
+						if [ -b "${bdev_disk}" ]
+						then
+							bdev=${bdev_disk}
+						else
+							if ! [ -e "${bdev_disk}" ]
+							then
+								log_warning_msg "${log_tag}: block device '${bdev_disk}' does not exist" 1>&2
+							else
+								log_warning_msg "${log_tag}: '${bdev_disk}' is not a block device" 1>&2
+							fi
+						fi
+						;;
+				esac
+			done
+		elif [ "x${blkid_opt}" = "x" ]
+		then
+			bdev=$(echo "${dev_dir}/${bdev}")
+		fi
+	fi
+
+	echo "${bdev}"
+}
 
 case "${1}" in
 	prereqs)
@@ -14,7 +96,7 @@ case "${1}" in
 		exit 0 ;;
 esac
 
-. /usr/share/initramfs-tools/scripts/functions
+. scripts/functions
 
 for opt in $(cat /proc/cmdline)
 do
@@ -28,12 +110,25 @@ if ! [ "x${optval}" = "x" ]
 then
 	for bdev in $(IFS=','; echo ${optval})
 	do
+		ignore=""
 		case "${bdev}" in
 			-*)
-				list_ignore=${list_ignore:+"${list_ignore} "}${bdev#-} ;;
-			*)
-				list_lock=${list_lock:+"${list_lock} "}${bdev} ;;
+				bdev=${bdev#-}
+				ignore="y" 
+				;;
 		esac
+
+		bdev=$(parse_bdev "${bdev}")
+
+		if ! [ "x${bdev}" = "x" ]
+		then
+			if [ "x${ignore}" = "xy" ]
+			then
+				list_ignore=${list_ignore:+"${list_ignpore} "}${bdev}
+			else
+				list_lock=${list_lock:+"${list_lock} "}${bdev}
+			fi
+		fi
 	done
 fi
 
@@ -41,83 +136,30 @@ if [ -f "${conf_file}" ]
 then
 	while IFS= read -r bdev
 	do
-		for blkdev in ${list_ignore}
-		do
-			case "${blkdev}" in
-				${bdev})
-					continue 2 ;;
-			esac
-		done
- 
-		list_lock=${list_lock:+"${list_lock} "}${bdev}
+		bdev=$(parse_bdev "${bdev}")
+
+		if ! [ "x${bdev}" = "x" ]
+		then
+			list_lock=${list_lock:+"${list_lock} "}${bdev}
+		fi
 	done <<-EOF
 	$(grep -v '^[[:blank:]]*\(#\|$\)' "${conf_file}")
 	EOF
 fi
 
-for blkdev in ${list_lock}
+for bdev in ${list_lock}
 do
-	bdev_disk=""
-	case "${blkdev}" in
-		disk=*)
-			bdev_disk="y"
-			blkdev=${blkdev#disk=} ;;
-	esac
+	for blkdev in ${list_ignore}
+	do
+		case "${bdev}" in
+			${blkdev})
+				continue 2 ;;
+		esac
+	done
 
-	bdev=""
-	case "${blkdev}" in
-		UUID=*)
-			bdev=$(blkid -U "${blkdev#UUID=}") ;;
-		LABEL=*)
-			bdev=$(blkid -L "${blkdev#LABEL=}") ;;
-		/*)
-			bdev=${blkdev} : ;;
-		*)
-			bdev=${udev_dir}/${blkdev} : ;;
-	esac
-
-	if [ ${?} -ne 0 ]
+	if ! blockdev --setro "${bdev}"
 	then
-		log_warning_msg "${tag}: failed to get block device for '${blkdev}'"
-		continue
-	fi
-
-	if ! [ -e "${bdev}" ]
-	then
-		log_warning_msg "${tag}: '${bdev}' does not exist"
-		continue
-	fi
-
-	if ! [ -b "${bdev}" ]
-	then
-		log_warning_msg "${tag}: '${bdev}' is not a block device"
-		continue
-	fi
-
-	if [ "x${bdev_disk}" = "xy" ]
-	then
-		bdev_disk=""
-
-		for block in $(cd /sys/block; echo *)
-		do
-			case "${bdev##*/}" in
-				${block}*)
-					bdev_disk=${block} ;;
-			esac
-
-			if [ "x${bdev_disk}" != "x" -a -d "/sys/block/${bdev_disk}/${bdev}" -a -b "${udev_dir}/${bdev_disk}" ]
-			then
-				break
-			fi
-		done
-
-		bdev=${bdev_disk:+"${udev_dir}/${bdev_disk}"}
-	fi
-
-	[ "x${bdev}" != "x" ] && echo blockdev --setro "${bdev}"
-	if [ ${?} -ne 0 ]
-	then
-		log_warning_msg "${tag}: failed to set '${bdev}' read-only"
+		log_warning_msg "${log_tag}: failed to set '${bdev}' read-only" 1>&2
 		continue
 	fi
 done
